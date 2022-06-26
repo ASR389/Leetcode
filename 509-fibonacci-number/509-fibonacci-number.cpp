9fd449c515bfcc0618059c94ff570a00b4f9b1b0
AJAY SINGH RAJAWAT <97617151+ASR389@users.noreply.github.com>
2022-06-26 19:15:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (79.75%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int solve(int n, vector<int>&dp){
+    int solve(int n){
         //base case
-        dp[0] = 0;
-        dp[1] = 1;
-  //tabulation method          
+        int prev1 = 1;
+        int prev2 = 0;
+  //tabulation method  with space optimization        
         for(int i = 2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n];
+        return prev1;
     }
-    int fib(int n) {
-        vector<int>dp(n+1);
-        
+    int fib(int n) {        
         if(n == 0) return 0; //yeh dhyan rakh lena ki 0 ho toh reutrn 0 kr de
-        int ans = solve(n,dp);
+        int ans = solve(n);
         
         return ans;
         
