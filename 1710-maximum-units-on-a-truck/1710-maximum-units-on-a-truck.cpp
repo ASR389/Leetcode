892331a1084755e9a7cc74a936d3f776bae313df
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-07-01 18:33:17 +0530

    Time: 60 ms (65.45%), Space: 15.8 MB (99.74%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    static bool sorting(vector<int>& a, vector<int>&b)
-    {
-        return a[1] > b[1];
-    }
-    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(), boxTypes.end(), sorting);
-        int units=0;
-        int boxes=0;
-        int i=0;
-        while(boxes<=truckSize && i<boxTypes.size())
-        {
-           if (boxes+boxTypes[i][0] <= truckSize)
-            {
-                boxes += boxTypes[i][0];
-                units += boxTypes[i][1]*boxTypes[i][0];
-            }
-            else
-            {
-                units += (truckSize-boxes)*boxTypes[i][1];
-                break;
-            }
-            i++;
+    int maximumUnits(vector<vector<int>>& boxes, int truckSize) {
+        // support variables
+        int res = 0, currBatch;
+        // sorting boxes by decreasing capacity
+        sort(begin(boxes), end(boxes), [](auto &a, auto &b){return a[1] > b[1];});
+		// carrying as many larger sized boxes as we can first
+        for (auto &el: boxes) {
+            currBatch = min(el[0], truckSize);
+            truckSize -= currBatch;
+            res += currBatch * el[1];
+            if (!truckSize) break;
         }
-        return units;
+        return res;
     }
 };
