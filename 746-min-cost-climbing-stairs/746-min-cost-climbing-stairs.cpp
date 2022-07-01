06162a06003cbfb7a16e363f35122cb72699cb20
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-07-02 00:18:33 +0530

    Time: 12 ms (40.79%), Space: 13.7 MB (51.39%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dp[1001];
+    int find(int n, vector<int>& cost){
+        if(n>=cost.size())
+            return 0;
+        if(dp[n]!=-1)
+            return dp[n];
+        int t1 = find(n+1, cost);
+        int t2 = find(n+2, cost);
+        return dp[n]= cost[n]+min(t1, t2);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        for( int i=0; i<1001; i++) dp[i]=-1;
+        return min(find(0,cost), find(1,cost));
+    }
+};
