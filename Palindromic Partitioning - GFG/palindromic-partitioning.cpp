007957e430a9ef1829ce5c02ea5325bed2440e70
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-07-15 19:46:28 +0530

    Added solution - LeetHub

--- Palindromic Partitioning - GFG/palindromic-partitioning.cpp
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -9,33 +9,36 @@ using namespace std;
 
 class Solution{
 public:
-bool isPalin(string &s,int i,int j){
-   while(i<j){
-       if(s[i]!=s[j])return false;
-       i++;
-       j--;
-   }
-    return true;
-}
-int dp[501][501];
-int solve(string &s,int i,int j){
-    if(i>=j)return 0;
-    if(isPalin(s,i,j))return 0;
-    if(dp[i][j]!=-1)return dp[i][j];
-    int ans=INT_MAX;
-    for(int k=i;k<j;k++){
-        
-            int temp=solve(s,i,k)+1+solve(s,k+1,j);
-            ans=min(ans,temp);
-        
+    int dp[501][501]; 
+    bool isPalindrome(string &str,int i,int j){
+        while(str[i]==str[j]){
+            i++;
+            j--;
+        if(i>j) return true;
+     }
+     return false;
     }
-    return dp[i][j]=ans;
-}
-    int palindromicPartition(string s)
+     
+    int solve(string &str, int i , int j){
+        // base case
+        if(i>=j) return 0;
+        if(isPalindrome(str,i,j)) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        // MCM conditions
+        int ans = INT_MAX;
+        for(int k = i; k<=j-1; k++){
+            int temp =  solve(str,i,k) + 1 + solve(str, k+1, j);
+            ans = min(ans,temp);
+        }
+        return dp[i][j] = ans;
+    }
+    int palindromicPartition(string str)
     {
-     memset(dp,-1,sizeof(dp));
-     int n=s.length();
-     return solve(s,0,n-1);
+        memset(dp,-1,sizeof(dp));
+        int n = str.length();
+        return solve(str,0,n-1);
     }
 };
 
