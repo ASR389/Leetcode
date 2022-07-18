bcb38b6e70358f2b49c2e4325056299e668ae148
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-07-18 19:19:37 +0530

    Time: 14 ms (68.50%), Space: 20.2 MB (92.86%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,18 +11,23 @@
  */
 class Solution {
 public:
-    int diameter(TreeNode * root, int &ans)
-    {
-       if(root==NULL)
-            return 0;
-        int left = diameter(root->left ,ans );
-        int right = diameter(root->right,ans);
-        ans = max(ans, left+right);
-        return max(left,right)+1;
+    int solve(TreeNode* root, int& res){
+        //base case
+        if(root == NULL) return 0;
+        
+        int l = solve(root->left,res);
+        int r = solve(root->right,res);
+        
+        int temp = 1 + max(l,r);     //storing temp ki answer mere root se hoke ja raha hai
+        int ans = max(temp, 1+l+r);  //answer mere lst + rst +1 se aa raha hai leke temp se cmp kr lo
+        
+        res = max(res,ans);  //final answer res mai store kara do jo max aa raha ho
+        
+        return temp;  // l,r diameter calculate krne k liye
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        int result=0,temp=0;
-        temp= diameter(root,result);
-        return result; 
+        int res = INT_MIN;
+        solve(root,res);
+        return res-1;
     }
 };
