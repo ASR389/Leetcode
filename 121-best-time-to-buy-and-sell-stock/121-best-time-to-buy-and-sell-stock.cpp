887567ff5fb5627c7bbc2472781c8f66896e86cd
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-07-31 00:11:03 +0530

    Time: 217 ms (42.77%), Space: 93.4 MB (52.42%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int minSoFar = INT_MAX;
+        int maxProfit = 0;
+        
+        for(int i =0; i<prices.size(); i++){
+            minSoFar = min(minSoFar,prices[i]);
+            maxProfit = max(maxProfit,prices[i] - minSoFar);
+        }
+        return maxProfit;
+    }
+};
