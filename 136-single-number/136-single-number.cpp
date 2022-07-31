54e78d63c3e7ebeb4338f7b1fd002a0dbafb7742
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-08-01 01:12:31 +0530

    Time: 43 ms (26.60%), Space: 17 MB (52.06%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) { 
-       unordered_map<int,int> a;
-	   for(auto x: nums)
-		   a[x]++;
-	   for(auto z:a)
-		   if(z.second==1)
-			   return z.first;
-	   return -1;
+    int singleNumber(vector<int>& arr) {
+        int n = arr.size(); // extracting the size of the array
+        
+        // traverse from the array
+        for(int i = 0; i < n - 1; i++)
+        {
+            arr[i + 1] = arr[i] ^ arr[i + 1]; // (prev answer xor current index)
+        }
+        
+        return arr[n- 1]; // return left over element
     }
 };
