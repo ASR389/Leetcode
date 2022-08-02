8df36957c2b7f3ee357383e6247f418c1dc65926
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-08-03 00:37:39 +0530

    Time: 142 ms (92.58%), Space: 85.4 MB (71.47%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        vector<vector<int>>ans;
-        if (m * n != original.size()) return {};
         
-        for (int i = 0; i < m*n;) {
-            vector<int> temp;
-            for (int j = 0; j < n; j++)
-                temp.push_back(original[i++]);
-            
-            ans.push_back(temp);
-        }
+         if (m * n != original.size()) return {};   
+        
+        vector<vector<int>> ans(m, vector<int>(n,0));
+        
+        for(int i=0;i<original.size();i++)
+		    ans[i/n][i%n] = original[i];
+        
         return ans;
     }
 };
