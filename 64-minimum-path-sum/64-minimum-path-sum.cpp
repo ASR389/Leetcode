07776aa7884d4d0ff13edb94841f6a9941b5c237
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-08-07 04:16:34 +0530

    Time: 11 ms (80.93%), Space: 9.9 MB (63.22%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -3,26 +3,20 @@ public:
     int dp[205][205];
     int solve(int m,int n,vector<vector<int>>&grid){
         //base case
-        if(m==0 and n==0) return grid[m][n];
+        if(m==grid.size()-1 and n==grid[0].size()-1) return grid[m][n];
+        
+        if(m == grid.size() || n == grid[0].size()) return INT_MAX;
 		
 		// dp->memorization
         if(dp[m][n]!=-1) return dp[m][n];
-		
-        int a = INT_MAX;
-        if(m>0) 
-            a = grid[m][n]+solve(m-1,n,grid);
-        
-        int b = INT_MAX;
-        if(n>0) 
-            b = grid[m][n]+solve(m,n-1,grid);
-        
-        return dp[m][n]=min((a),(b));
+
+        return dp[m][n]=grid[m][n] + min(solve(m+1,n,grid),solve(m,n+1,grid));
     }
     int minPathSum(vector<vector<int>>& grid) {
         memset(dp,-1,sizeof(dp));
         int m = grid.size()-1;
         int n = grid[0].size()-1;
         
-        return solve(m,n,grid);
+        return solve(0,0,grid);
     }
 };
