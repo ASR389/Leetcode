081355e9c1a7a8f835175012410fe3e935a6ac0a
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-09-19 16:25:58 +0530

    Time: 1940 ms (12.76%), Space: 9.1 MB (77.06%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int diff = target;  //just for understanding i change the names
-     // the problem will be same as count the # subset in a given diff
-     // we will sove this like previos problem
-        int n = nums.size();
-        int sum = 0;
-        for(int i=0; i<n; i++) sum+=nums[i];
-        
-        if((sum+diff)%2 !=0 || target > sum) return 0;
-        
-        int s1 = abs((diff+sum)/2);   //for -ve number remember for abs
-        
-        //now count subset sum  means count s1 possibility
-        int dp[n+1][s1+1];
-        
-        for(int i=0;i<s1+1;i++) dp[0][i] = 0;
-        for(int i=0;i<n+1;i++) dp[i][0] = 1;
-        
-        for(int i=1;i<n+1;i++){
-            for(int j=0;j<s1+1;j++){
-                if(nums[i-1]<=j)
-                dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
-                else
-                    dp[i][j] = dp[i-1][j];
+    void solve(vector<int>& nums, int target, int i, int &count, int sum){
+        if(i == nums.size()){
+            if(target == sum){
+                count++;
             }
+            return;
         }
-        return dp[n][s1];
         
+        int curr = nums[i];
+        
+        solve(nums, target, i+1, count, sum+curr);
+        solve(nums, target, i+1, count, sum-curr);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+        
+        int index = 0;
+        int count = 0;
+        int sum = 0;
+        solve(nums, target, index, count, sum);
+        return count;
     }
 };
