bcbcc2f250a62e6a45118d7395ff2bbbede57471
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-11-16 12:13:32 +0530

    Time: 3 ms (35.43%), Space: 5.9 MB (67.50%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -7,44 +7,44 @@
  * int guess(int num);
  */
 
-// class Solution {
-// public:
-//     int guessNumber(int n) {
-//         int start = 1;
-//         int end  = n;
-//         int ans;
-        
-//         while(start<=end){
-//            int mid = (start + end)/2;
-//            int pick = guess(mid);
-            
-//             if(pick == 1){
-//                 start = mid+1;
-//             }
-//             else if(pick == -1){
-//                 end = mid-1;
-//             }
-//            else if(pick ==0){
-//                return mid;
-//             }   
-//         }
-//         return -1;      
-//     }
-// };
-
 class Solution {
 public:
     int guessNumber(int n) {
-        int l=1;
-        int r=n;
-        while(l<=n){
-            int mid=r-((r-l)/2);
-            int num=guess(mid);
-            if(num==0) return mid;
-            else if(num==-1) r=mid-1;
-            else l=mid+1;
+        int start = 1;
+        int end  = n;
+        int ans;
+        
+        while(start<=end){
+           int mid =end - ((end-start)/2);
+           int pick = guess(mid);
+            
+            if(pick == 1){
+                start = mid+1;
+            }
+            else if(pick == -1){
+                end = mid-1;
+            }
+           else if(pick ==0){
+               return mid;
+            }   
         }
-        return -1;
-   
+        return -1;      
     }
 };
+
+// class Solution {
+// public:
+//     int guessNumber(int n) {
+//         int l=1;
+//         int r=n;
+//         while(l<=n){
+//             int mid=r-((r-l)/2);
+//             int num=guess(mid);
+//             if(num==0) return mid;
+//             else if(num==-1) r=mid-1;
+//             else l=mid+1;
+//         }
+//         return -1;
+   
+//     }
+// };
