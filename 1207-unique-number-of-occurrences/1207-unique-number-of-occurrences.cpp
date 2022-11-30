fbcf00dde1f472ff23fd7fafa668cb7f58908b4a
AJAY SINGH RAJAWAT <97617151+AjayRajawat01@users.noreply.github.com>
2022-11-30 23:18:24 +0530

    Time: 3 ms (86.20%), Space: 8.3 MB (20.75%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        vector<int> v;
-        int size = arr.size();
-
-        sort(arr.begin(), arr.end());
-        
-        int i=0;
-        while(i<size) {
-            int count = 1;
-            for(int j=i+1; j<size; j++){
-                if(arr[i] == arr[j]) {
-                    count++;
-                } else {
-                    break;
-                }
-            }
-            v.push_back(count);
-            i += count;
+        unordered_map<int, int>mp;
+        for(auto i : arr){
+            mp[i]++;
         }
-        
-        size = v.size();
-        sort(v.begin(), v.end());
-        
-        for(i=0; i<size-1; i++) {
-            if(v[i] == v[i+1]) {
-                return false;
-            }
+        set<int>st;
+        for(auto x : mp){
+            st.insert(x.second);
         }
-        return true;
-        
+        if(mp.size() == st.size()) return true;
+        return false;
     }
 };
